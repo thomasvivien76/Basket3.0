7d1992c6604a8b2671cd238399a298410f3ffd50
thomasvivien76 <thomasvivien76@gmail.com>
2025-07-02 22:00:11 +0200

    ajout diaporama

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,3 +8,45 @@ document.getElementById('close-menu').addEventListener('click', function() {
     nav.classList.remove('active');
 });
 
+/*diapo*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    // Cache toutes les diapositives
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Incrémente l'index de la diapositive et revient à la première si nécessaire
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+
+    // Retire la classe "active" de tous les points
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Affiche la diapositive courante et active le point correspondant
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    // Change de diapositive toutes les 3 secondes (3000 millisecondes)
+    setTimeout(showSlides, 6000);
+}
+
+// Fonctions pour naviguer manuellement
+function plusSlides(n) {
+    slideIndex += n - 1; // Ajuste l'index pour la fonction showSlides
+    showSlides();
+}
+
+function currentSlide(n) {
+    slideIndex = n - 1; // Ajuste l'index pour la fonction showSlides
+    showSlides();
+}
+
